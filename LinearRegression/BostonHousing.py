a849ed91d92211007b666d828db0b3965d2920c6
satyaad-kore <satyaaditya.baratam@kore.com>
2018-12-25 00:56:08 +0530

    added plotting logic

--- LinearRegression/BostonHousing.py
+++ LinearRegression/BostonHousing.py
@@ -1,5 +1,7 @@
 #importing linear regression
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot  as plt
 
 from sklearn import datasets
 import numpy as np
@@ -11,34 +13,41 @@ def linear_regression():
     data = datasets.load_boston()
 
     #take dataset and load it in padas dataframe and setting independent variables
-    independent_data_frame = pd.DataFrame(data.data[:-10], columns=data.feature_names)
+    independent_data_frame = pd.DataFrame(data.data, columns=data.feature_names )#data.feature_names)
 
     # print list(independent_data_frame.columns.values)
-    print data.feature_names
-
-    test_data_frame = pd.DataFrame(data.data[-10:], columns=data.feature_names)
+    # print data.feature_names
 
     #load dependent or target variables in another data frame
-    dependent_data_frame = pd.DataFrame(data.target[:-10], columns=['MEDV'])
+    dependent_data_frame = pd.DataFrame(data.target, columns=['MEDV'])
 
-    print list(dependent_data_frame.columns.values)
+    # print list(dependent_data_frame.columns.values)
 
     # y = mX + c
     X = independent_data_frame
     y = dependent_data_frame['MEDV']
 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    print X_train.shape, y_train.shape
+    print X_test.shape, y_test.shape
     # ----------------------- fit data in to model ------------------------------------------------
 
     # instantiate Linear regression class
     linear_regression = linear_model.LinearRegression()
 
     #fit data in to linear model
-    linear_regression.fit(X, y)
+    linear_regression.fit(X_train, y_train)
 
 
     # ------------------------ make predicitions --------------------------------------------------
-    predictions = linear_regression.predict(test_data_frame)
-    print predictions[:5]
+    predictions = linear_regression.predict(X_test)
+    print predictions[:10]
+
+
+    plt.scatter(y_test, predictions, edgecolors='red')
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
+    plt.show()
     # R sqr
     print linear_regression.score(X, y)
     print linear_regression.coef_
